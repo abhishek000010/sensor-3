d0f49e11e748c3f70736d7f993ba51cba2b19524
abhishek000010 <abhisheksingh17171@gmail.com>
2023-09-13 01:42:12 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name ="src",
+    version="0.0.1",
+    author="Abhishek",
+    author_email="abhisheksingh17171@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
